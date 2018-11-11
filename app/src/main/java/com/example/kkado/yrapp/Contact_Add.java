84f2b79735b45e98918060bd78025d9db02f77aa
Kyle <kad16001@byui.edu>
2018-11-10 18:27:04 -0700

    Created a "Contact_Book" class/activity and add contact. Yes, contact_book is almost identical to activity_template. Contact_View page will look very similar to Contact_Add. These constriants are driving me nuts!!!

---
+++ app/src/main/java/com/example/kkado/yrapp/Contact_Add.java
@@ -0,0 +1,13 @@
+package com.example.kkado.yrapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Contact_Add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact__add);
+    }
+}
